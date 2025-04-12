0e6b30c7efe7b63870a86100e56156bb38521295
anushevanam <anush@evanam.com>
2025-04-12 16:48:46 +1000

    Added Timestamps

--- models/Project.js
+++ models/Project.js
@@ -3,5 +3,6 @@ const projectSchema = new mongoose.Schema({
   name: { type: String, required: true },
   description: String,
   owner: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
+},{timestamps: true
 });
 module.exports = mongoose.model('Project', projectSchema);
